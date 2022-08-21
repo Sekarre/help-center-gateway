49220a6b31457946843b5fa7c700448d16729fb4
Sekarre <zimny2121@gmail.com>
2022-08-21 13:35:33 +0200

    Moved routes to class, instead of yml file

--- src/main/java/com/sekarre/helpcentergateway/config/RoutesConfig.java
+++ src/main/java/com/sekarre/helpcentergateway/config/RoutesConfig.java
@@ -5,36 +5,48 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import static org.springframework.cloud.gateway.filter.factory.DedupeResponseHeaderGatewayFilterFactory.Strategy.RETAIN_FIRST;
+import static org.springframework.http.HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS;
+import static org.springframework.http.HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN;
+
 @Component
 public class RoutesConfig {
 
-//    @Bean
-//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-//        return builder.routes()
-//                .route("core", r -> r.path("/api/v1/issues/**", "/api/v1/comments/**", "/api/v1/users/**")
-//                        .filters(f-> f.circuitBreaker(config -> config
-//                                .setName("fallback-core")
-//                                .setFallbackUri("forward:/default-fallback")
-//                        ))
-//                        .uri("lb://help-center-core"))
-//                .route("auth", r -> r.path("/api/v1/auth/**")
-//                        .filters(f-> f.circuitBreaker(config -> config
-//                                .setName("fallback-auth")
-//                                .setFallbackUri("forward:/default-fallback")
-//                        ))
-//                        .uri("lb://help-center-auth"))
-//                .route("chat-info", r -> r.path("/api/v1/chat-info/**", "/websocket/**")
-//                        .filters(f-> f.circuitBreaker(config -> config
-//                                .setName("fallback-chat")
-//                                .setFallbackUri("forward:/default-fallback")
-//                        ))
-//                        .uri("lb://help-center-chat"))
-//                .route("notification", r -> r.path("/api/v1/event-notifications/**")
-//                        .filters(f-> f.circuitBreaker(config -> config
-//                                .setName("fallback-notification")
-//                                .setFallbackUri("forward:/default-fallback")
-//                        ))
-//                        .uri("lb://help-center-notification"))
-//                .build();
-//    }
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("core", r -> r.path("/api/v1/issues/**", "/api/v1/comments/**", "/api/v1/users/**")
+                        .filters(f-> f
+                                .dedupeResponseHeader(ACCESS_CONTROL_ALLOW_CREDENTIALS, RETAIN_FIRST.name())
+                                .dedupeResponseHeader(ACCESS_CONTROL_ALLOW_ORIGIN, RETAIN_FIRST.name())
+                                .circuitBreaker(config -> config
+                                        .setName("fallback-core")
+                                        .setFallbackUri("forward:/default-fallback")))
+                        .uri("lb://help-center-core"))
+                .route("auth", r -> r.path("/api/v1/auth/**")
+                        .filters(f-> f
+                                .dedupeResponseHeader(ACCESS_CONTROL_ALLOW_CREDENTIALS, RETAIN_FIRST.name())
+                                .dedupeResponseHeader(ACCESS_CONTROL_ALLOW_ORIGIN, RETAIN_FIRST.name())
+                                .circuitBreaker(config -> config
+                                        .setName("fallback-auth")
+                                        .setFallbackUri("forward:/default-fallback")))
+                        .uri("lb://help-center-auth"))
+                .route("chat", r -> r.path("/api/v1/chat-info/**", "/websocket/**")
+                        .filters(f-> f
+                                .dedupeResponseHeader(ACCESS_CONTROL_ALLOW_CREDENTIALS, RETAIN_FIRST.name())
+                                .dedupeResponseHeader(ACCESS_CONTROL_ALLOW_ORIGIN, RETAIN_FIRST.name())
+                                .circuitBreaker(config -> config
+                                        .setName("fallback-chat")
+                                        .setFallbackUri("forward:/default-fallback")))
+                        .uri("lb://help-center-chat"))
+                .route("notification", r -> r.path("/api/v1/event-notifications/**")
+                        .filters(f-> f
+                                .dedupeResponseHeader(ACCESS_CONTROL_ALLOW_CREDENTIALS, RETAIN_FIRST.name())
+                                .dedupeResponseHeader(ACCESS_CONTROL_ALLOW_ORIGIN, RETAIN_FIRST.name())
+                                .circuitBreaker(config -> config
+                                        .setName("fallback-notification")
+                                        .setFallbackUri("forward:/default-fallback")))
+                        .uri("lb://help-center-notification"))
+                .build();
+    }
 }
