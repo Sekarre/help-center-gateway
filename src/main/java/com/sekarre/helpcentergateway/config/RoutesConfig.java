4d6b2265568de1457b91fd5f96113f4d9a9c06d0
Sekarre <zimny2121@gmail.com>
2022-08-21 13:12:32 +0200

    Added JWT authentication

--- src/main/java/com/sekarre/helpcentergateway/config/RoutesConfig.java
+++ src/main/java/com/sekarre/helpcentergateway/config/RoutesConfig.java
@@ -8,33 +8,33 @@ import org.springframework.stereotype.Component;
 @Component
 public class RoutesConfig {
 
-    @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route("core", r -> r.path("/api/v1/issues/**", "/api/v1/comments/**", "/api/v1/users/**")
-                        .filters(f-> f.circuitBreaker(config -> config
-                                .setName("fallback-core")
-                                .setFallbackUri("forward:/default-fallback")
-                        ))
-                        .uri("lb://help-center-core"))
-                .route("auth", r -> r.path("/api/v1/auth/**")
-                        .filters(f-> f.circuitBreaker(config -> config
-                                .setName("fallback-auth")
-                                .setFallbackUri("forward:/default-fallback")
-                        ))
-                        .uri("lb://help-center-auth"))
-                .route("chat-info", r -> r.path("/api/v1/chat-info/**", "/websocket/**")
-                        .filters(f-> f.circuitBreaker(config -> config
-                                .setName("fallback-chat")
-                                .setFallbackUri("forward:/default-fallback")
-                        ))
-                        .uri("lb://help-center-chat"))
-                .route("notification", r -> r.path("/api/v1/event-notifications/**")
-                        .filters(f-> f.circuitBreaker(config -> config
-                                .setName("fallback-notification")
-                                .setFallbackUri("forward:/default-fallback")
-                        ))
-                        .uri("lb://help-center-notification"))
-                .build();
-    }
+//    @Bean
+//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route("core", r -> r.path("/api/v1/issues/**", "/api/v1/comments/**", "/api/v1/users/**")
+//                        .filters(f-> f.circuitBreaker(config -> config
+//                                .setName("fallback-core")
+//                                .setFallbackUri("forward:/default-fallback")
+//                        ))
+//                        .uri("lb://help-center-core"))
+//                .route("auth", r -> r.path("/api/v1/auth/**")
+//                        .filters(f-> f.circuitBreaker(config -> config
+//                                .setName("fallback-auth")
+//                                .setFallbackUri("forward:/default-fallback")
+//                        ))
+//                        .uri("lb://help-center-auth"))
+//                .route("chat-info", r -> r.path("/api/v1/chat-info/**", "/websocket/**")
+//                        .filters(f-> f.circuitBreaker(config -> config
+//                                .setName("fallback-chat")
+//                                .setFallbackUri("forward:/default-fallback")
+//                        ))
+//                        .uri("lb://help-center-chat"))
+//                .route("notification", r -> r.path("/api/v1/event-notifications/**")
+//                        .filters(f-> f.circuitBreaker(config -> config
+//                                .setName("fallback-notification")
+//                                .setFallbackUri("forward:/default-fallback")
+//                        ))
+//                        .uri("lb://help-center-notification"))
+//                .build();
+//    }
 }
